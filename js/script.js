bdcf225571ef96dcac3884d8bfcce511719cbe33
parix1999 <parisottoedoardo@gmail.com>
2021-05-27 15:13:26 +0200

    password-1.0

--- js/script.js
+++ js/script.js
@@ -2,3 +2,6 @@ var name = prompt('Inserisci il tuo nome');
 var surname = prompt('Inserisci il tuo cognome');
 var color = prompt('Inserisci il tuo colore preferito');
 
+var totale = (name) + (surname) + (color) + '21';
+
+document.getElementById('references').innerHTML= totale;
