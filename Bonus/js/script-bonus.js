577acdf55c9a9b723a99ba23f93ddbee5ed1ccc2
parix1999 <parisottoedoardo@gmail.com>
2021-05-27 16:06:56 +0200

    bonus

--- Bonus/js/script-bonus.js
+++ Bonus/js/script-bonus.js
@@ -1,5 +1,16 @@
 alert('Ciao sono javascript e sono al tuo servizio');
-var titolo = prompt('Inserisci il tuo nome');
+var titoloName = prompt('Come ti chiami ?');
+var surname = prompt("Qual'è il tuo cognome ?");
+var color = prompt("Qual'è il tuo colore preferito ?");
+var age = prompt("Quanti anni hai?");
 
-document.getElementById('title').innerHTML = 'Benvenuto '+titolo;
+document.getElementById('title').innerHTML = 'Benvenuto '+titoloName;
+
+document.getElementById('name').innerHTML = titoloName;
+
+document.getElementById('surname').innerHTML = surname;
+document.getElementById('color').innerHTML = color;
+document.getElementById('age').innerHTML = age;
+
+
 
