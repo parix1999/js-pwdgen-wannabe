d76ed4dabddad0f4819d953ef21733b6f3f663eb
parix1999 <parisottoedoardo@gmail.com>
2021-05-27 15:47:24 +0200

    Bonus

---
+++ Bonus/js/script-bonus.js
@@ -0,0 +1,5 @@
+alert('Ciao sono javascript e sono al tuo servizio');
+var titolo = prompt('Inserisci il tuo nome');
+
+document.getElementById('title').innerHTML = 'Benvenuto '+titolo;
+
--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-var name = prompt('Inserisci il tuo nome');
+var nome = prompt('Inserisci il tuo nome');
 var surname = prompt('Inserisci il tuo cognome');
 var color = prompt('Inserisci il tuo colore preferito');
 
-var totale = (name) + (surname) + (color) + '21';
+var totale = nome + surname + color + '21';
 
 document.getElementById('references').innerHTML= totale;
